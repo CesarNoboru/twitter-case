68e1d31816130f5d6430f6b925d0580926bd5c66
CesarNoboru <cesar.noboru@hotmail.com>
2020-12-20 15:37:25 -0300

    No more ECR + zip deployment

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM 249615491021.dkr.ecr.us-east-1.amazonaws.com/twitter-case-repo:latest
-
-USER root
-WORKDIR /app
-COPY ./app/ .
-RUN apk add python3 py3-pip &&\
-    python3 -m pip install --upgrade python-dotenv pymysql requests urllib3 boto3
-
-ENTRYPOINT python3 main.py
--- app/main.py
+++ app/main.py
@@ -3,15 +3,16 @@ from log import logger
 import rds
 import time
 from twitter import search
-import os
 
-try:
-    logger.info("msg='Starting search for tags.'")
-    start_time = time.time()
-    tags = read_tags('TAGS')      
-    data = search(tags)
-    rds.run(data)
-    seconds = time.time() - start_time
-    logger.info(f"msg='Job done' exec_time={seconds:.2f}")
-except Exception as e:
-    raise e
+def handler(event, context):
+    try:
+        logger.info("msg='Starting search for tags.'")
+        start_time = time.time()
+        tags = read_tags('TAGS')      
+        data = search(tags)
+        rds.run(data)
+        seconds = time.time() - start_time
+        logger.info(f"msg='Job done' exec_time={seconds:.2f}")
+    except Exception as e:
+        raise e
+    return 0
